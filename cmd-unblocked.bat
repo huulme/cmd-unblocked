a046c51ed7dbf7dedaf48eb338137b474b6816d4
James Hulme <220141387+huulme@users.noreply.github.com>
2025-11-20 03:39:32 +0000

    wow im so smart

--- cmd-unblocked.bat
+++ cmd-unblocked.bat
@@ -6,9 +6,8 @@ for /f "skip=1 delims=" %%i in ('ver') do (
     echo %%i
 )
 echo (c) Microsoft Corporation. All rights reserved.
-echo.
 :loop
+echo.
 set /p input="%cd%>"
 call %input%
-echo.
 goto loop
